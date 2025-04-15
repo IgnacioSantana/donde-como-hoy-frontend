dcafaa008e7b72b3966d88cb567c0d53cf6928e5
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-15 16:12:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    fs: {
+      allow: ['.']
+    }
+  },
+  // 👇 ESTA ES LA CLAVE
+  resolve: {
+    alias: {
+      '/@': '/src',
+    }
+  },
+  base: '/',
 })
