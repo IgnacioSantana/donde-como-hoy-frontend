61e0ece4272d583f8aa6bca75f6d574eded73899
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-16 00:16:16 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App'
-import Signup from './pages/Signup'
-import TestRegistro from './pages/TestRegistro';
-import Panel from './pages/Panel'; // arriba con los imports
-import RutaProtegida from './components/RutaProtegida';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
+import App from './App';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Panel from './pages/Panel';
+import RutaProtegida from './components/RutaProtegida';
+import TestRegistro from './pages/TestRegistro'; // si lo seguís usando
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,9 +16,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/panel" element={<Panel />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/panel"
+          element={
+            <RutaProtegida>
+              <Panel />
+            </RutaProtegida>
+          }
+        />
         <Route path="/test" element={<TestRegistro />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
