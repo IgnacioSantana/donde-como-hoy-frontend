ae2b444167c36a99d568e75fc4b437bf9a77657b
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-15 23:46:37 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-
 import App from './App'
 import Signup from './pages/Signup'
 import TestRegistro from './pages/TestRegistro';
+import Panel from './pages/Panel'; // arriba con los imports
+
+// Dentro de <Routes>...
+<Route path="/panel" element={<Panel />} />
 
 <Route path="/test" element={<TestRegistro />} />
 
