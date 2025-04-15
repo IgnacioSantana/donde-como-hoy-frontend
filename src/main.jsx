9fb8d79b55111d7ce2b01442647abd51b85f733f
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-15 17:30:28 +0200

    Activar rutas con React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App'
-import Signup from './pages/Signup';
-
-<Route path="/signup" element={<Signup />} />
-
+import Signup from './pages/Signup'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
