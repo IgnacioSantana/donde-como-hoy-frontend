623e39377567899d449e309b0ec0419a2fff3f7d
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-16 00:08:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,14 @@ import Signup from './pages/Signup'
 import TestRegistro from './pages/TestRegistro';
 import Panel from './pages/Panel'; // arriba con los imports
 
-// Dentro de <Routes>...
-<Route path="/panel" element={<Panel />} />
-
-<Route path="/test" element={<TestRegistro />} />
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/panel" element={<Panel />} />
+        <Route path="/test" element={<TestRegistro />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
