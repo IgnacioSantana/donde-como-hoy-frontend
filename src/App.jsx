97c84fd7604cb2c852d00e25e12e437cefb6efb4
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-15 17:46:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="text-center mt-10">
+      <img src="/logo.png" alt="Logo" className="mx-auto w-48 mb-6" />
+      <div className="space-x-4">
+        <a href="/login">Iniciar sesión</a>
+        <a href="/signup">Registra tu restaurante</a>
+      </div>
+    </div>
   );
 }
