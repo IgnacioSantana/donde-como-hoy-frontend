022c084058ce115c02e49737600b340c3ac85309
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-17 12:50:51 +0200

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Signup() {
   const [formulario, setFormulario] = useState({
