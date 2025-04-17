5a8754e4c7dfe18d34da8a5a00b8214d682656df
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-17 12:40:45 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [formulario, setFormulario] = useState({
