7e08bacd1276df4d467cc6a39d856ffc585c0ece
IgnacioSantana <62885677+IgnacioSantana@users.noreply.github.com>
2025-04-17 17:39:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,4 +9,33 @@ import Login from './pages/Login';
 import Panel from './pages/PanelRestaurante';
 import RutaProtegida from './components/RutaProtegida';
 import CrearMenu from './pages/CrearMenu';
-import TestRegistro from './pages/TestRegistro'; // ✅ este era el que faltaba
+import TestRegistro from './pages/TestRegistro'; // ✅ ¡Ya está!
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/panel"
+          element={
+            <RutaProtegida>
+              <Panel />
+            </RutaProtegida>
+          }
+        />
+        <Route
+          path="/crear-menu"
+          element={
+            <RutaProtegida>
+              <CrearMenu />
+            </RutaProtegida>
+          }
+        />
+        <Route path="/test" element={<TestRegistro />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
